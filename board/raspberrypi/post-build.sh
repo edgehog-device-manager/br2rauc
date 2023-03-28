367e54fb3ebdf7dc093c82e1096c609f82d1b7c2
Joshua Chapman <joshua.chapman@secomind.com>
2023-03-28 10:59:55 +0200

    fix(docker): use rootfs-overlay to link the directory to writable paths

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -11,21 +11,21 @@ RAUC_VERSION=${VERSION:-${BR2_VERSION_FULL}}
 
 # Add a console on tty1
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab ||
-        sed -i '/GENERIC_SERIAL/a\
+    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
 # Mount persistent data partitions
 if [ -e ${TARGET_DIR}/etc/fstab ]; then
-    # For configuration data
-    # WARNING: data=journal is safest, but potentially slow!
-    grep -qE 'LABEL=Data' ${TARGET_DIR}/etc/fstab ||
-        echo "LABEL=Data /var/lib ext4 defaults,data=journal,noatime 0 0" >>${TARGET_DIR}/etc/fstab
+	# For configuration data
+	# WARNING: data=journal is safest, but potentially slow!
+	grep -qE 'LABEL=Data' ${TARGET_DIR}/etc/fstab || \
+	echo "LABEL=Data /var/lib ext4 defaults,data=journal,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
 
-    # For bulk data (eg: firmware updates)
-    grep -qE 'LABEL=OTA' ${TARGET_DIR}/etc/fstab ||
-        echo "LABEL=OTA /var/tmp ext4 defaults,noatime 0 0" >>${TARGET_DIR}/etc/fstab
+	# For bulk data (eg: firmware updates)
+	grep -qE 'LABEL=OTA' ${TARGET_DIR}/etc/fstab || \
+	echo "LABEL=OTA /var/tmp ext4 defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
 fi
 
 # Copy custom cmdline.txt file
@@ -33,32 +33,21 @@ install -D -m 0644 ${BR2_EXTERNAL_BR2RAUC_PATH}/board/raspberrypi/cmdline.txt ${
 
 # Copy RAUC certificate
 if [ -e ${BR2_EXTERNAL_BR2RAUC_PATH}/openssl-ca/dev/ca.cert.pem ]; then
-    install -D -m 0644 ${BR2_EXTERNAL_BR2RAUC_PATH}/openssl-ca/dev/ca.cert.pem ${TARGET_DIR}/etc/rauc/keyring.pem
+	install -D -m 0644 ${BR2_EXTERNAL_BR2RAUC_PATH}/openssl-ca/dev/ca.cert.pem ${TARGET_DIR}/etc/rauc/keyring.pem
 else
-    echo "RAUC CA certificate not found!"
-    echo "...did you run the openssl-ca.sh script?"
-    exit 1
+	echo "RAUC CA certificate not found!"
+	echo "...did you run the openssl-ca.sh script?"
+	exit 1
 fi
 
 # Update RAUC compatible string
 sed -i "/compatible/s/=.*\$/=${RAUC_COMPATIBLE}/" ${TARGET_DIR}/etc/rauc/system.conf
 
-# HACK: to make the directory writable since they are used by docker, this can be done in the
-#       overlay but it is easier to do it here
-mkdir -vp "${TARGET_DIR}/var/lib/opt"
-rm -vr "${TARGET_DIR}/var/lib/etc-docker" || true
-if [ -d "${TARGET_DIR}/etc/docker" ]; then
-    mv -vf "${TARGET_DIR}/etc/docker/" "${TARGET_DIR}/var/lib/etc-docker"
-fi
-rm -vrf "${TARGET_DIR}/opt" || true
-ln -vsf ../var/lib/opt "${TARGET_DIR}/opt"
-ln -vsf ../../var/lib/etc-docker "${TARGET_DIR}/etc/docker"
-
 # Create rauc version file
-echo "${RAUC_VERSION}" >${TARGET_DIR}/etc/rauc/version
+echo "${RAUC_VERSION}" > ${TARGET_DIR}/etc/rauc/version
 
 # Customize login prompt with login hints
-cat <<-EOF >>${TARGET_DIR}/etc/issue
+cat <<- EOF >> ${TARGET_DIR}/etc/issue
 
 	Default username:password is [user:<empty>]
 	Root login disabled, use sudo su -
